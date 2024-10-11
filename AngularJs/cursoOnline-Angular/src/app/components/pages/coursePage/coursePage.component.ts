6844ce21828aae32313dde37cba098a8932521a3
Alvaro <alvaroppm01@gmail.com>
2024-10-11 20:24:16 +0200

    Final project

--- AngularJs/cursoOnline-Angular/src/app/components/pages/coursePage/coursePage.component.ts
+++ AngularJs/cursoOnline-Angular/src/app/components/pages/coursePage/coursePage.component.ts
@@ -1,15 +1,33 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
-import { RouterLink } from "@angular/router";
+import { RouterLink, ActivatedRoute } from "@angular/router";
 @Component({
     selector: 'coursePage',
-    standalone: true,
-    imports: [CommonModule, RouterLink],
+    standalone: true, 
+    imports: [CommonModule, RouterLink], 
     templateUrl: './coursePage.component.html',
     styleUrls: ['./coursePage.component.css']
 })
 
 export class CoursePage implements OnInit {
+    courseId: string = '';  
+    constructor(private route: ActivatedRoute) {}
+
     ngOnInit(): void {
+        this.route.paramMap.subscribe((params) => {
+          this.courseId = params.get('id') || '';
+        });
+    }
+
+    estaEnCurso1(): boolean {
+        return this.courseId === '1';
+    }
+
+    estaEnCurso2(): boolean {
+        return this.courseId === '2';
+    }
+
+    estaEnCurso3(): boolean {
+        return this.courseId === '3';
     }
 }
--- AngularJs/cursoOnline-Angular/src/app/components/pages/profilePage/profilePage.component.ts
+++ AngularJs/cursoOnline-Angular/src/app/components/pages/profilePage/profilePage.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'profilePage',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterLink],
     templateUrl: './profilePage.component.html',
     styleUrls: ['./profilePage.component.css']
 })
